fe1a67e8ae5747bd7415575547deae8aa7522c3c
FlorHid <40501083+FlorHid@users.noreply.github.com>
2021-01-28 05:40:06 -0300

    pipes
    uploadingPipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  name       : string   = 'Capitan America';
+  name2      : string   = 'CapItAn aMeRIca';
+  array      : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI         : number   = Math.PI;
+  percentage : number   = 0.234;
+  salary     : number   = 1234.5;
+  currentDate: Date     = new Date();
+  activate   : boolean  = true;
+
+  language   : string = 'es';
+  videoUrl   : string = 'https://www.youtube.com/embed/fqBNnC_apV4';
+
+  valuePromise = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('data works');
+    }, 4500);
+  });
+
+  heroe = {
+    name: 'Logan',
+    codename: 'Wolverine',
+    age: '500',
+    direction: {
+      street: 'First',
+      house: 20
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from "@angular/common";
+import localeEs from "@angular/common/locales/es";
+import localeFr from "@angular/common/locales/fr";
+
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+
 
 import { AppComponent } from './app.component';
+import { CapitalizedPipe } from './pipes/capitalized.pipe';
+import { DomsecurePipe } from './pipes/domsecure.pipe';
+import { PasswordPipe } from './pipes/password.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizedPipe,
+    DomsecurePipe,
+    PasswordPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
